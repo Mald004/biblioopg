6e85d7794b043df11eb32b552bebac91076bbc47
mald004 <cph-mm737@cphbusiness.dk>
2023-02-22 16:49:11 +0100

    Complete: 2/22/2023 niko undervisning

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import Entitet.User;
+import Language.Dansk;
+import Language.Dialog;
+import Language.English;
+import Util.Input;
+import dataLister.Facade;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dialog dialog;
+
+        dialog = new Dansk();
+        System.out.println(dialog.hello());
+
+        dialog = new English();
+
+        System.out.println(dialog.hello());
+
+        System.out.println(Input.getInt(dialog.askAge()));
+
+        dialog = new Dansk();
+
+
+        while(true){
+
+            System.out.println(Input.getInt(dialog.askAge()));
+            if(Input.getString(dialog.changeLanguage()).equalsIgnoreCase("y")){
+                if(dialog instanceof Dansk){
+                    dialog = new English();
+                }else
+                if(dialog instanceof English){
+                    dialog = new Dansk();
+                }
+            }
+        }
     }
+
 }
